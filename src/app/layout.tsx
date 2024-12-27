b4e48f90cf057ea3090ebb5c835b109d29725ff0
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-27 03:01:23 -0500

    Update layout with global stylesheets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "@scss/core.scss";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "@scss/soaricons.scss";
+import "@scss/main.scss";
 
 export const metadata: Metadata = {
   title: "SOAR Inc.",
@@ -24,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         {children}
       </body>
     </html>
