4404b8f8327e2ce6e4bc17454b365a966aee4470
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-27 23:19:38 -0500

    fix(logo): Add prop for additional classes

--- src/ui/components/Logo.tsx
+++ src/ui/components/Logo.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { Icon } from "./Icon";
 
-const Logo = () => {
+type LogoProps = {
+	className?: string
+}
+
+const Logo: React.FC<LogoProps> = ({ className }) => {
+	const extraClass = 'undefined' !== typeof className && '' !== className
+		? ' ' + className
+		: '';
+
 	return (
-		<h1 className="soarui-logo">
+		<h1 className={`soarui-logo${ extraClass }`}>
 			<Icon name="task" className="soarui-logo__icon" />
 			<span className="soarui-logo__name">Soar Task</span>
 		</h1>
